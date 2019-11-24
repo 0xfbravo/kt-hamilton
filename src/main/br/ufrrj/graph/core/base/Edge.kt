1fff1f01f78cfee299137c2eb883bb22a74030f4
insidemybrain <fellipe.bravo@gmail.com>
2019-11-24 08:52:39 -0300

    Added gradle support

--- src/main/br/ufrrj/graph/core/base/Edge.kt
+++ src/main/br/ufrrj/graph/core/base/Edge.kt
@@ -1,9 +1,9 @@
 package br.ufrrj.graph.core.base
 
 abstract class Edge<VertexValueType>(val id: String,
-                                   val initialVertex: Vertex<VertexValueType>,
-                                   val arrivalVertex: Vertex<VertexValueType>,
-                                   val cost: Double) {
+                                     val initialVertex: Vertex<VertexValueType>,
+                                     val arrivalVertex: Vertex<VertexValueType>,
+                                     val cost: Double) {
     /**
      * Checks if the current edge has access
      * to the vertex passed as parameter.
